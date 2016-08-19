16337b5b2970393776e7b5b64fe407e2af2060e4
Sergey Mironchuk <sergey@mironchuk.in.ua>
2016-08-19 17:47:22 +0300

    Initial load

---
+++ Microsoft Graph Training/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Web.Mvc;
+using Microsoft_Graph_SDK_ASPNET_Connect.Controllers.Http;
+using Newtonsoft.Json;
+
+namespace Microsoft_Graph_SDK_ASPNET_Connect.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string _httpsLocalhost = "https://localhost:44300";
+        private string _clientId = "cec5e543-f6e6-4cac-b8e2-c60ef2d346ed";
+        private string _clientSecret = "nnCrOsLJeiEA2EXDR3tMFFScYAc9/KIbKTaqZ+Qk2xw=";
+        private string _resourceUrl = "https://graph.microsoft.com/";
+        private string _httpsLoginMicrosoftonlineCom = "https://login.microsoftonline.com";
+
+        public ActionResult Index(string code, string command)
+        {
+            command = command ?? "me";
+            AuthorizationToken token = Session["GraphToken"] as AuthorizationToken;
+
+            if ((token == null || token.ExpirationDateTime <= DateTimeOffset.Now) && string.IsNullOrEmpty(code))
+            {
+                Request.Cookies.Clear();
+                Response.Cookies.Clear();
+                return Redirect($"https://login.microsoftonline.com/common/oauth2/authorize?response_type=code&redirect_uri={_httpsLocalhost}&client_id={_clientId}");
+            }
+
+            if (token == null)
+            {
+                token = GetAuthorizationToken(code);
+                Session["GraphToken"] = token;
+            }
+
+            if (!string.IsNullOrEmpty(code)) return RedirectToAction("Index");
+
+            var userInfo = RunGraphCommand("me", token);
+            var user = JsonConvert.DeserializeObject<UserAzure>(userInfo);
+
+            var result = RunGraphCommand(command, token);
+
+            ViewBag.Email = JsonPrettify(result);
+            ViewBag.Token = token.Token;
+            ViewBag.Command = command;
+            ViewBag.UserName = user.UserPrincipalName;
+
+            return View("Graph");
+        }
+
+        private string RunGraphCommand(string command, AuthorizationToken token)
+        {
+            var headers = new HeaderParameters
+            {
+                { "Authorization", $"Bearer {token.Token}"},
+                {"Accept-Charset", "UTF-8" }
+            };
+            var requestManager = new RestRequestManager(new Uri(_resourceUrl));
+            var result = requestManager.Get(
+                $"v1.0/{command.TrimStart('/')}",
+                HttpRequestContentTypes.Application.Json,
+                headers);
+            return result;
+        }
+
+        private AuthorizationToken GetAuthorizationToken(string code)
+        {
+            var requestManager = new RestRequestManager(new Uri(_httpsLoginMicrosoftonlineCom));
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                new KeyValuePair<string, string>("redirect_uri", _httpsLocalhost),
+                new KeyValuePair<string, string>("client_id", _clientId),
+                new KeyValuePair<string, string>("client_secret", _clientSecret),
+                new KeyValuePair<string, string>("code", code),
+                new KeyValuePair<string, string>("resource", _resourceUrl),
+            });
+
+            var token = requestManager.Post<AuthorizationToken>(
+                "common/oauth2/token",
+                "application/json",
+                new HeaderParameters(),
+                content);
+
+            return token;
+        }
+
+        private static string JsonPrettify(string json)
+        {
+            using (var stringReader = new StringReader(json))
+            using (var stringWriter = new StringWriter())
+            {
+                var jsonReader = new JsonTextReader(stringReader);
+                var jsonWriter = new JsonTextWriter(stringWriter) { Formatting = Formatting.Indented };
+                jsonWriter.WriteToken(jsonReader);
+                return stringWriter.ToString();
+            }
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult SignOut()
+        {
+            Session["GraphToken"] = null;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SignIn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class UserAzure
+    {
+        public string UserPrincipalName { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
---
+++ Microsoft Graph Training/Controllers/Http/RequestParameters.cs
@@ -0,0 +1,60 @@
+// Copyright (c) 2016, Virtuworks.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must  retain  the  above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// - Redistributions in binary form  must  reproduce the  above  copyright  notice,
+//   this list of conditions  and  the  following  disclaimer in  the documentation
+//   and/or other materials provided with the distribution.
+//
+// - Neither  the  name  of  Virtuworks   nor   the   names  of  its
+//   contributors may be used to endorse or  promote  products  derived  from  this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT  NOT  LIMITED TO, THE IMPLIED
+// WARRANTIES  OF  MERCHANTABILITY   AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL,  SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT  OF  SUBSTITUTE  GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  HOWEVER  CAUSED AND ON
+// ANY  THEORY  OF  LIABILITY,  WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING  IN  ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft_Graph_SDK_ASPNET_Connect.Controllers.Http
+{
+    public class RequestParameters : Dictionary<string,string>
+    {
+        public override string ToString()
+        {
+            return ((object)this.ToStringBuilder()).ToString();
+        }
+
+        private StringBuilder ToStringBuilder()
+        {
+            var builder = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> keyValuePair in (Dictionary<string, string>) this)
+            {
+                AddKeyValueString(builder, keyValuePair.Key, keyValuePair.Value);
+            }
+
+            return builder;
+        }
+
+        protected void AddKeyValueString(StringBuilder builder, string key, string value)
+        {
+            string str = builder.Length == 0 ? string.Empty : "&";
+            builder.AppendFormat("{0}{1}=", (object)str, (object)key);
+            builder.Append(value);
+        }
+    }
+}
